13da35471279fd4625da45fcdc8f841e39e5191e
Leah Silber <leahsilber@gmail.com>
2019-09-03 06:44:01 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -40,14 +40,14 @@ class Song
   end
 
   def self.count
-    @@count
+    @@count.uniq
   end
 
   def self.artists
-    @@artists
+    @@artists.uniq
   end
 
   def self.genres
-    @@genres
+    @@genres.uniq
   end
 end
