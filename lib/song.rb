12e39020605559affe15d934fa318f52517a2318
Leah Silber <leahsilber@gmail.com>
2019-09-03 05:36:45 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,11 +2,18 @@ class Song
 
   attr_accessor :artists, :genres, :name
 
+  @@genre_count = 0
+  @@genre_artists = []
+  @@genre_roster = {}
+
+  @@artist_count = 0
+  @@artist_genre = []
+  @@artist_roster = {}
+
   def initialize(name, artist, genre)
     # @name = name
     # @artist = artist
     # @genre = genre
-
     @name, @artist, @genre = name, artist, genre
 
   end
